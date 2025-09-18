c9942e797eb09443b71455c63421d03353fef700
DominikDO98 <dominiklas13@wp.pl>
2025-09-18 15:51:16 +0200

    check

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 export const API_URL = import.meta.env.VITE_API_URL;
-export const CLIENT_ULR = import.meta.env.VITE_CLIENT_URL;
+export const CLIENT_URL = import.meta.env.VITE_CLIENT_URL;
 export const HEADERS = {
-  "access-control-allow-origin": `${CLIENT_ULR}`,
+  "access-control-allow-origin": `${CLIENT_URL}`,
   "content-type": "application/json",
 };
